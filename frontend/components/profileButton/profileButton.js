bc39a35ce146068a1f3631137ad61ff41e932ef5
ulianacode <ul1ana.4@yandex.ru>
2025-04-03 12:31:10 +0300

    feat/(VOYAGO-46):fix name of screens

--- frontend/components/profileButton/profileButton.js
+++ frontend/components/profileButton/profileButton.js
@@ -9,7 +9,6 @@ const ProfileButton = ({ title, onPress}) => {
       style={styles.button}
       onPress={onPress}
     >
-      
     <View style={styles.battonContainer}>
     <Text style={styles.text}>{title}</Text>
         <Image
