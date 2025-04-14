713fabbb64bcc205ba44e8e561fdfceacd6c8b13
ulianacode <165685604+ulianacode@users.noreply.github.com>
2025-04-14 15:52:49 +0300

    feature/(VOYAGO-80): template of main screen
    * feat(VOYAGO-27): init
    
    * feat(VOYAGO-28): add template of authorization screen
    
    * fix(VOYAGO-28):fix keyboard appear (#18)
    
    * feat(VOYAGO-30):add template of premium and free premium
    
    * feat/(VOYAGO-47):add template of filters
    
    * feat/(VOYAGO-47): add template of filters
    
    * feat/(VOYAGO-54): add template preview of route
    
    * fix/(VOYAGO-78): fix button search city
    
    * fix/(VOYAGO-70): fix button continue on preference screen
    
    * feat/(VOYAGO-50): add template of additional parameters
    
    * feat/(VOYAGO-84): add template of profile notifications
    
    * feat/(VOYAGO-83): template of alert delete profile
    
    * feat/(VOYAGO-82): template of alert cancel premium
    
    * feat/(VOYAGO-80): template of main screen
    
    ---------
    
    Co-authored-by: Арсений <126655451+qudest@users.noreply.github.com>

--- frontend/components/ProfileButton/ProfileButton.js
+++ frontend/components/ProfileButton/ProfileButton.js
@@ -3,14 +3,14 @@ import { TouchableOpacity, View, Image, Text, StyleSheet } from 'react-native';
 import styles from './styles';
 
 
-const ProfileButton = ({ title, onPress}) => {
+const ProfileButton = ({ title, onPress, enabled=true}) => {
   return (
     <TouchableOpacity 
       style={styles.button}
       onPress={onPress}
     >
     <View style={styles.battonContainer}>
-    <Text style={styles.text}>{title}</Text>
+    <Text style={enabled ? styles.text : styles.disabledText}>{title}</Text>
         <Image
             source={require('../../assets/profileImages/nagivation.png')}
             style={styles.imageNavigation}
