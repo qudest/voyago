5da0018f51ba7572aabffcd46ac074d19a886d98
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2025-04-19 11:16:06 +0300

    feat: routeService

---
+++ backend/route-service/src/main/java/by/smertex/service/RouteService.java
@@ -0,0 +1,27 @@
+package by.smertex.service;
+
+import by.smertex.core.dto.input.RouteCreateOrUpdateDto;
+import by.smertex.core.dto.input.RouteFilter;
+import by.smertex.core.dto.output.RouteReadDto;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface RouteService {
+
+    RouteReadDto findById(Long id);
+
+    List<RouteReadDto> findAll(RouteFilter filter, Pageable pageable);
+
+    List<RouteReadDto> findAllFavorites(Long userId);
+
+    RouteReadDto create(RouteCreateOrUpdateDto dto);
+
+    RouteReadDto update(Long id, RouteCreateOrUpdateDto dto);
+
+    void addToFavorites(Long routeId, Long userId);
+
+    void delete(Long id);
+
+    void rate(Long userId, Long rating);
+}
---
+++ backend/route-service/src/main/java/by/smertex/service/impl/RouteServiceImpl.java
@@ -0,0 +1,58 @@
+package by.smertex.service.impl;
+
+import by.smertex.core.dto.input.RouteCreateOrUpdateDto;
+import by.smertex.core.dto.input.RouteFilter;
+import by.smertex.core.dto.output.RouteReadDto;
+import by.smertex.service.RouteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RouteServiceImpl implements RouteService {
+
+    @Override
+    public RouteReadDto findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<RouteReadDto> findAll(RouteFilter filter, Pageable pageable) {
+        return List.of();
+    }
+
+    @Override
+    public List<RouteReadDto> findAllFavorites(Long userId) {
+        return List.of();
+    }
+
+    @Override
+    public RouteReadDto create(RouteCreateOrUpdateDto dto) {
+        return null;
+    }
+
+    @Override
+    public RouteReadDto update(Long id, RouteCreateOrUpdateDto dto) {
+        return null;
+    }
+
+    @Override
+    public void addToFavorites(Long routeId, Long userId) {
+
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    @Override
+    public void rate(Long userId, Long rating) {
+
+    }
+
+
+}
