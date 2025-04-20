d4eb8d913eda9c34542a9bdbbacff74ee7051579
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2025-04-21 00:32:55 +0300

    feat: fix db

--- backend/rating-service/src/main/resources/db/changelog/v-1.0.0/19-04-25-rating-table.sql
+++ backend/rating-service/src/main/resources/db/changelog/v-1.0.0/19-04-25-rating-table.sql
@@ -12,5 +12,6 @@ CREATE TABLE ratings.ratings
     id           BIGINT PRIMARY KEY DEFAULT nextval('user_seq'),
     element_id   BIGINT  NOT NULL,
     rating       INTEGER NOT NULL,
-    from_user_id BIGINT  NOT NULL UNIQUE
+    from_user_id BIGINT  NOT NULL,
+    CONSTRAINT unique_element_user UNIQUE (element_id, from_user_id)
 );
