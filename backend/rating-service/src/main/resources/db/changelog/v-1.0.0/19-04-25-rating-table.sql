f7830bb15d45f07ee89eeda05bbb39f0ea495683
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2025-04-20 16:29:00 +0300

    feat: liquid base

---
+++ backend/rating-service/src/main/resources/db/changelog/v-1.0.0/19-04-25-rating-table.sql
@@ -0,0 +1,16 @@
+--liquibase formatted sql
+
+--changeset qudest:1
+CREATE SCHEMA IF NOT EXISTS ratings;
+
+--changeset qudest:2
+CREATE SEQUENCE IF NOT EXISTS user_seq START WITH 1 INCREMENT BY 3;
+
+--changeset qudest:3
+CREATE TABLE ratings.ratings
+(
+    id           BIGINT PRIMARY KEY DEFAULT nextval('user_seq'),
+    element_id   BIGINT  NOT NULL,
+    rating       INTEGER NOT NULL,
+    from_user_id BIGINT  NOT NULL UNIQUE
+);
