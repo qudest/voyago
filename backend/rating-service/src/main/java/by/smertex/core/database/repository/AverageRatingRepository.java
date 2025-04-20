b454dcf01ebeeccff46a8d6b032519cec7628d32
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2025-04-21 00:35:15 +0300

    feat: change AverageRatingRepository ID type from String to Long

--- backend/rating-service/src/main/java/by/smertex/core/database/repository/AverageRatingRepository.java
+++ backend/rating-service/src/main/java/by/smertex/core/database/repository/AverageRatingRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AverageRatingRepository extends CrudRepository<AverageRating, String> {
+public interface AverageRatingRepository extends CrudRepository<AverageRating, Long> {
 }
