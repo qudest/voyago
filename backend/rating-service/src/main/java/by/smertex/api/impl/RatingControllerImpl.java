b6ef091da108eb3784c3943ca51cf5e8b1e2896f
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2025-04-20 16:28:39 +0300

    feat: implement rating controller

---
+++ backend/rating-service/src/main/java/by/smertex/api/impl/RatingControllerImpl.java
@@ -0,0 +1,27 @@
+package by.smertex.api.impl;
+
+import by.smertex.api.RatingController;
+import by.smertex.core.dto.AverageRatingDto;
+import by.smertex.core.dto.RatingDto;
+import by.smertex.core.service.RatingService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/ratings")
+@RequiredArgsConstructor
+public class RatingControllerImpl implements RatingController {
+
+    private final RatingService ratingService;
+
+    @GetMapping
+    public AverageRatingDto getRating(@RequestParam Long routeId) {
+        return ratingService.getAverageRating(routeId);
+    }
+
+    @PostMapping
+    public RatingDto create(@RequestParam Long userId, @Valid @RequestBody RatingDto ratingDto) {
+        return ratingService.create(userId, ratingDto);
+    }
+}
