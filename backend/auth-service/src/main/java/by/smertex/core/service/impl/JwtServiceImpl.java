26f496fd2b582e63e852453473a086e04a2ac495
Smertex <86659869+Smertex@users.noreply.github.com>
2025-04-26 01:41:02 +0300

    Feature/(voyago 62)notification service (#49)
    * fix: fix structure in auth-service
    
    * fix: docker-compose fix

--- backend/auth-service/src/main/java/by/smertex/core/service/impl/JwtServiceImpl.java
+++ backend/auth-service/src/main/java/by/smertex/core/service/impl/JwtServiceImpl.java
@@ -1,23 +1,18 @@
 package by.smertex.core.service.impl;
 
 import by.smertex.core.database.model.impl.Token;
-import by.smertex.core.database.repository.TokenRepository;
 import by.smertex.core.dto.input.AccountReadDto;
 import by.smertex.core.dto.input.PhoneCodeDto;
-import by.smertex.core.dto.input.RefreshTokenDto;
+import by.smertex.core.dto.output.JwtClaims;
 import by.smertex.core.dto.output.TokenDto;
-import by.smertex.core.exception.GenerateJwtException;
-import by.smertex.core.exception.InvalidRefreshException;
 import by.smertex.core.mapper.Mapper;
 import by.smertex.core.service.FindAccountService;
 import by.smertex.core.service.JwtService;
 import by.smertex.core.service.PhoneCodeService;
 import by.smertex.core.util.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -27,48 +22,70 @@ public class JwtServiceImpl implements JwtService {
 
     private final FindAccountService findAccountService;
 
-    private final TokenRepository tokenRepository;
+    // private final TokenRepository tokenRepository;
 
     private final Mapper<Token, TokenDto> tokenDtoMapper;
 
     private final JwtUtil<AccountReadDto> jwtAccessUtil;
 
-    private final JwtUtil<String> jwtRefreshUtil;
+    // private final JwtUtil<String> jwtRefreshUtil;
 
+    @Override
     public TokenDto generateToken(PhoneCodeDto dto) {
         phoneCodeService.verifyCode(dto);
-        return Optional.ofNullable(findAccountService.findAccountByPhoneNumber(dto.phoneNumber()))
-                .map(account ->
-                        tokenRepository.save(
-                                Token.builder()
-                                        .phoneNumber(account.phoneNumber())
-                                        .accessToken(jwtAccessUtil.generateToken(account))
-                                        .refreshToken(jwtRefreshUtil.generateToken(account.phoneNumber()))
-                                        .build()
-                        )
+        String token = jwtAccessUtil.generateToken(
+                findAccountService.findAccountByPhoneNumber(
+                        dto.phoneNumber()
                 )
-                .map(tokenDtoMapper::map)
-                .orElseThrow(() -> new GenerateJwtException("Generate jwt exception", HttpStatus.BAD_REQUEST.value()));
-    }
-
-    public TokenDto updateToken(RefreshTokenDto dto) {
-        jwtRefreshUtil.validateToken(dto.refreshToken());
-        String username = jwtRefreshUtil.getUsername(dto.refreshToken());
-        return tokenRepository.findById(username)
-                .map(entity -> {
-                    entity.setAccessToken(
-                            jwtAccessUtil.generateToken(
-                                    findAccountService.findAccountByPhoneNumber(username)
-                            )
-                    );
-                    return tokenRepository.save(entity);
-                })
-                .map(tokenDtoMapper::map)
-                .orElseThrow(() -> new InvalidRefreshException("Invalid refresh token", HttpStatus.UNAUTHORIZED.value()));
+        );
+        return TokenDto.builder()
+                .accessToken(token)
+                .build();
     }
 
     @Override
-    public void removeToken(String phoneNumber) {
-        tokenRepository.deleteById(phoneNumber);
+    public JwtClaims validateToken(String token) {
+        jwtAccessUtil.validateToken(token);
+        return JwtClaims.builder()
+                .name(jwtAccessUtil.getUsername(token))
+                .role(jwtAccessUtil.getRole(token))
+                .build();
     }
+
+//    public TokenDto generateToken(PhoneCodeDto dto) {
+//        phoneCodeService.verifyCode(dto);
+//        return Optional.ofNullable(findAccountService.findAccountByPhoneNumber(dto.phoneNumber()))
+//                .map(account ->
+//                        tokenRepository.save(
+//                                Token.builder()
+//                                        .phoneNumber(account.phoneNumber())
+//                                        .accessToken(jwtAccessUtil.generateToken(account))
+//                                        .refreshToken(jwtRefreshUtil.generateToken(account.phoneNumber()))
+//                                        .build()
+//                        )
+//                )
+//                .map(tokenDtoMapper::map)
+//                .orElseThrow(() -> new GenerateJwtException("Generate jwt exception", HttpStatus.BAD_REQUEST.value()));
+//    }
+
+//    public TokenDto updateToken(RefreshTokenDto dto) {
+//        jwtRefreshUtil.validateToken(dto.refreshToken());
+//        String username = jwtRefreshUtil.getUsername(dto.refreshToken());
+//        return tokenRepository.findById(username)
+//                .map(entity -> {
+//                    entity.setAccessToken(
+//                            jwtAccessUtil.generateToken(
+//                                    findAccountService.findAccountByPhoneNumber(username)
+//                            )
+//                    );
+//                    return tokenRepository.save(entity);
+//                })
+//                .map(tokenDtoMapper::map)
+//                .orElseThrow(() -> new InvalidRefreshException("Invalid refresh token", HttpStatus.UNAUTHORIZED.value()));
+//    }
+//
+//    @Override
+//    public void removeToken(String phoneNumber) {
+//        tokenRepository.deleteById(phoneNumber);
+//    }
 }
