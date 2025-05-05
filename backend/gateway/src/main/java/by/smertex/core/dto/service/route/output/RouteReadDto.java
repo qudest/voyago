ae639438b9160949c743ac9206883ba25f66bbbf
Арсений <126655451+qudest@users.noreply.github.com>
2025-05-05 23:57:41 +0300

    Update RouteReadDto.java

--- backend/gateway/src/main/java/by/smertex/core/dto/service/route/output/RouteReadDto.java
+++ backend/gateway/src/main/java/by/smertex/core/dto/service/route/output/RouteReadDto.java
@@ -2,18 +2,22 @@ package by.smertex.core.dto.service.route.output;
 
 import by.smertex.core.dto.service.route.Tag;
 import by.smertex.core.dto.service.route.input.RoutePoints;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
-public record RouteReadDto(
-        Long id,
-        String name,
-        Long createdBy,
-        List<Tag> tags,
-        RoutePoints routePoints,
-        Long distance,
-        Long duration,
-        Float rating
-) {
-
+@Getter
+@Setter
+@Builder
+public class RouteReadDto {
+    private Long id;
+    private String name;
+    private Long createdBy;
+    private List<Tag> tags;
+    private RoutePoints routePoints;
+    private Long distance;
+    private Long duration;
+    private Float rating;
 }
