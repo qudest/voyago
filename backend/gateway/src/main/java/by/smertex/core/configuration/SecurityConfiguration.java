b7f4868caaf1b3fd2fd252b5270d0ecd6445b855
smertex <smertexx@gmail.com>
2025-05-05 13:58:34 +0300

    feat: add in memory authorization for swagger

--- backend/gateway/src/main/java/by/smertex/core/configuration/SecurityConfiguration.java
+++ backend/gateway/src/main/java/by/smertex/core/configuration/SecurityConfiguration.java
@@ -3,13 +3,22 @@ package by.smertex.core.configuration;
 import by.smertex.api.filter.JwtFilter;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -20,7 +29,29 @@ public class SecurityConfiguration {
 
     private final JwtFilter jwtFilter;
 
+    @Value("${in-memory.username}")
+    private String username;
+
+    @Value("${in-memory.password}")
+    private String password;
+
     @Bean
+    @Order(1)
+    SecurityFilterChain swaggerSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .securityMatcher("/swagger-ui/**", "/v3/api-docs/**")
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().hasRole("SWAGGER")
+                )
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .build();
+    }
+
+    @Bean
+    @Order(2)
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
@@ -46,9 +77,23 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager() {
-        return authentication -> {
-            throw new UnsupportedOperationException("No authentication should be done with this AuthenticationManager");
-        };
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails swaggerUser = User.builder()
+                .username(username)
+                .password(password)
+                .roles("SWAGGER")
+                .build();
+        return new InMemoryUserDetailsManager(swaggerUser);
+    }
+
+    @Bean
+    @SuppressWarnings("all")
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
