9061ec660d48baeb614bbda208810e6dd91764b2
Smertex <86659869+Smertex@users.noreply.github.com>
2025-05-22 00:13:06 +0300

    Feature/(voyago 166)nginx tls (#88)
    * fix: swagger cors
    
    ---------
    
    Co-authored-by: smertex <smertexx@gmail.com>

--- backend/gateway/src/main/java/by/smertex/core/configuration/SwaggerConfiguration.java
+++ backend/gateway/src/main/java/by/smertex/core/configuration/SwaggerConfiguration.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfiguration {
@@ -17,7 +20,10 @@ public class SwaggerConfiguration {
                 .components(new Components().addSecuritySchemes
                         ("Bearer Authentication", createAPIKeyScheme()))
                 .info(new Info().title("Voyago Gateway API")
-                        .description("Описание API Voyago"));
+                        .description("Описание API Voyago"))
+                .servers(List.of(
+                        new Server().url("https://voyago-app.ru")
+                ));
     }
 
     private SecurityScheme createAPIKeyScheme() {
