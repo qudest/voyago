9bf56c9559e3d230a6330630d02ae813a71b70b0
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2025-05-22 23:56:56 +0300

    fix: update query logic in RouteRepository to improve tag filtering and route point matching

--- backend/route-service/src/main/java/by/smertex/core/database/repository/RouteRepository.java
+++ backend/route-service/src/main/java/by/smertex/core/database/repository/RouteRepository.java
@@ -15,15 +15,18 @@ public interface RouteRepository extends JpaRepository<Route, Long> {
     List<Route> findByName(String name);
 
     @Query("""
-        SELECT r
-        FROM Route r
-        WHERE (:#{#dto.name} IS NULL OR LOWER(r.name) LIKE CONCAT('%', LOWER(:#{#dto.name}), '%'))
-          AND (:#{#dto.tags} IS NULL OR r.tags IN :#{#dto.tags})
-          AND (:#{#dto.routePoints} IS NULL OR r.routePoints IN :#{#dto.routePoints})
-          AND (:#{#dto.distanceFrom} IS NULL OR r.distance >= :#{#dto.distanceFrom})
-          AND (:#{#dto.distanceTo} IS NULL OR r.distance <= :#{#dto.distanceTo})
-          AND (:#{#dto.durationFrom} IS NULL OR r.duration >= :#{#dto.durationFrom})
-          AND (:#{#dto.durationTo} IS NULL OR r.duration <= :#{#dto.durationTo})
-       """)
+                SELECT r
+                FROM Route r
+                WHERE (:#{#dto.name} IS NULL OR LOWER(r.name) LIKE CONCAT('%', LOWER(:#{#dto.name}), '%'))
+                  AND (:#{#dto.tags} IS NULL OR EXISTS (
+                        SELECT t FROM r.tags t WHERE t IN :#{#dto.tags}
+                  ))
+                  AND (:#{#dto.routePoints.origin} IS NULL OR r.routePoints.origin = :#{#dto.routePoints.origin})
+                  AND (:#{#dto.routePoints.destination} IS NULL OR r.routePoints.destination = :#{#dto.routePoints.destination})
+                  AND (:#{#dto.distanceFrom} IS NULL OR r.distance >= :#{#dto.distanceFrom})
+                  AND (:#{#dto.distanceTo} IS NULL OR r.distance <= :#{#dto.distanceTo})
+                  AND (:#{#dto.durationFrom} IS NULL OR r.duration >= :#{#dto.durationFrom})
+                  AND (:#{#dto.durationTo} IS NULL OR r.duration <= :#{#dto.durationTo})
+            """)
     Slice<Route> findAllByFilter(RouteFilterDto dto, Pageable pageable);
 }
