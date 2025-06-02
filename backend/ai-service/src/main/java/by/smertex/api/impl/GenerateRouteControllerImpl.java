796f13906e017f3419d1f0b95cf1463991244891
Arseniy <nagornyi.arsenii@mail.ru>
2025-06-03 02:40:01 +0300

    feat: add AI service configuration and implement route generation endpoint

--- backend/ai-service/src/main/java/by/smertex/api/impl/GenerateRouteControllerImpl.java
+++ backend/ai-service/src/main/java/by/smertex/api/impl/GenerateRouteControllerImpl.java
@@ -5,7 +5,8 @@ import by.smertex.core.dto.input.GenerateRouteDto;
 import by.smertex.core.dto.output.RouteReadDto;
 import by.smertex.core.service.GenerateRouteService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +17,8 @@ public class GenerateRouteControllerImpl implements GenerateRouteController {
 
     private final GenerateRouteService generateRouteService;
 
-    @GetMapping
-    public RouteReadDto generateRoute(GenerateRouteDto dto) {
+    @PostMapping
+    public RouteReadDto generateRoute(@RequestBody GenerateRouteDto dto) {
         return generateRouteService.generateRoute(dto);
     }
 }
